734cd9c9c80060c22b0b3cbfff9c593c3cf3b929
yusfit <princefesomuyusuf@gmail.com>
2024-10-24 16:04:25 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+import Navbar from './components/Navbar/Navbar'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Shop from './pages/Shop'
+import ShopCategory from './pages/ShopCategory'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import LoginSignup from './pages/LoginSignup'
+
+function App() {
+  return (
+      <div>
+        <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Shop />}/>
+          <Route path='/mens' element={<ShopCategory category="men"/>}/>
+          <Route path='/womens' element={<ShopCategory category="women"/>}/>
+          <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+          <Route path='/product' element={<Product/>}>
+             <Route path=':productId' element={<Product/>} />    
+          </Route>
+          <Route path='/cart' element={<Cart />}/>
+          <Route path='/login' element={<LoginSignup />}/>
+        </Routes>     
+        </BrowserRouter>
+      </div>
+  )
+}
+
+export default App
